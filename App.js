f554a79a5c84568756173e0837fb03edad9fa9b8
Natalied9 <nataliedardaine@gmail.com>
2018-12-24 23:41:40 -0800

    Added style to list

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
+import ListItem from './src/components/ListItem/ListItem';
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
 
@@ -35,7 +36,7 @@ export default class App extends Component<Props> {
 
   render() {
     const placesOutput = this.state.places.map((place, i) => (
-      <Text key={i}>{place} </Text>
+      <ListItem key={i} placeName={place} />
      ));
 
     return (
